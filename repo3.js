006ea8f3b32b7532be330f2b5aae8cc694080081
Kokristof <kovacskristofattila@gmail.com>
2020-06-27 16:28:23 +0200

    newest

--- repo3.js
+++ repo3.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
- function doit(){ }
-  
-function addone(){
-  var foo= document.getElementById("over").innerHTML;
-  foo++;
-  document.getElementById("over").innerHTML=foo;}})
+var count=(function (){
+  var counter=0; return function(){return counter+=1;
+  }
+})();
+function displaycount(){
+  document.getElementById("carrier").innerHTML=count();
+}
 
     
